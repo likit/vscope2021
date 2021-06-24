ac07a0f4ec6c6cdaa5cdb13c5f646812d9b1a39f
Likit Preeyanon <likit.pre@mahidol.edu>
2021-06-25 05:43:13 +0700

    added programs and lessons pages for users

---
+++ src/views/user/LessonList.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <nav-menu :is-logged-in="isLoggedIn" @logout="isLoggedIn=false"></nav-menu>
+    <section class="section">
+      <div class="columns">
+        <div class="column is-three-fifths is-offset-2">
+          <div v-if="program !== null">
+            <h1 class="title has-text-centered">
+              โปรแกรม
+            </h1>
+            <h2 class="subtitle has-text-centered">
+              ชื่อ {{ program.name }}
+            </h2>
+          </div>
+          <b-table :data="lessons" :loading="isLoading">
+            <b-table-column field="name" label="ชื่อบทเรียน" v-slot="props">
+              {{ props.row.data.name }}
+            </b-table-column>
+            <b-table-column field="objective" label="วัตถุประสงค์" v-slot="props">
+              {{ props.row.data.objective }}
+            </b-table-column>
+            <b-table-column field="createdAt" label="เพิ่มเมื่อ" v-slot="props">
+              {{ props.row.data.createdAt.toDate().toLocaleString() }}
+            </b-table-column>
+            <b-table-column field="id" width="40" v-slot="props">
+              <a class="button is-info is-rounded"
+                 @click="$router.push({ name: 'LessonInfo', params: {programId: programId, lessonId: props.row.id}})">
+                <span class="icon">
+                    <i class="fas fa-chevron-circle-right"></i>
+                </span>
+                <span>Go</span>
+              </a>
+            </b-table-column>
+          </b-table>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-three-fifths is-offset-2">
+          <div class="buttons is-centered">
+            <button class="button is-light" @click="$router.push({ name: 'ProgramList' })">
+            <span class="icon">
+              <i class="fas fa-chevron-left"></i>
+            </span>
+              <span>Back</span>
+            </button>
+            <router-link class="button is-success"
+                         :to="{ name: 'LessonEditForm', params: { programId: programId }}">
+            <span class="icon">
+              <i class="fas fa-plus-circle"></i>
+            </span>
+              <span>เพิ่มบทเรียน</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+</template>
+
+<script>
+import {db, auth} from '../../firebase'
+import NavMenu from "../../components/navMenu";
+
+export default {
+  name: "UserLessonList",
+  components: {NavMenu},
+  data() {
+    return {
+      program: null,
+      programId: null,
+      lessons: [],
+      isLoggedIn: false,
+      isLoading: true
+    }
+  },
+  computed: {
+  },
+  beforeMount() {
+    let self = this
+    if (auth.currentUser) {
+      self.isLoggedIn = true
+    }
+    self.programId = self.$route.params.programId
+    db.collection('programs').doc(self.programId).get().then((snapshot)=>{
+      if (snapshot.exists) {
+        self.program = snapshot.data()
+      }
+    })
+    // TODO: add a filter for published lesson only
+    db.collection('lessons')
+        .where('programId', '==', self.programId).get().then((snapshot)=>{
+      snapshot.docs.forEach((d)=>{
+        self.lessons.push({
+          id: d.id,
+          data: d.data()
+        })
+      })
+      self.isLoading = false
+    })
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/user/ProgramList.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <nav-menu :is-logged-in="isLoggedIn" @logout="isLoggedIn=false"></nav-menu>
+    <section class="section">
+      <div class="columns">
+        <div class="column is-three-fifths is-offset-2">
+          <h1 class="title">All Programs</h1>
+          <b-table :data="programs" :loading="isLoading">
+            <b-table-column field="name" label="Name" width="40" v-slot="props">
+              {{ props.row.name }}
+            </b-table-column>
+            <b-table-column field="discipline" label="Discipline" width="40" v-slot="props">
+              {{ props.row.discipline }}
+            </b-table-column>
+            <b-table-column field="creator" label="Creator" width="40" v-slot="props">
+              {{ props.row.creator }}
+            </b-table-column>
+            <b-table-column field="createdAt" label="Created At" width="40" v-slot="props">
+              {{ props.row.createdAt }}
+            </b-table-column>
+            <b-table-column width="40" v-slot="props" label="Published">
+            <span class="icon" v-if="props.row.published">
+              <i class="fas fa-check-circle has-text-success"></i>
+            </span>
+            </b-table-column>
+            <b-table-column width="40" v-slot="props">
+              <a class="button is-info is-rounded"
+                 @click="$router.push({ name: 'UserLessonList', params: {programId: props.row.id}})">
+                <span class="icon">
+                  <i class="fas fa-chevron-circle-right"></i>
+                </span>
+                <span>Go</span>
+              </a>
+            </b-table-column>
+          </b-table>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-three-fifths is-offset-2">
+          <div class="buttons is-centered">
+            <router-link class="button is-light" :to="{ name: 'CreatorMain' }">
+            <span class="icon">
+              <i class="fas fa-chevron-left"></i>
+            </span>
+              <span>Back</span>
+            </router-link>
+            <router-link class="button is-success" :to="{ name: 'ProgramEditForm' }">
+            <span class="icon">
+              <i class="fas fa-plus-circle"></i>
+            </span>
+              <span>Add</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import NavMenu from "../../components/navMenu";
+import {db, auth} from '../../firebase'
+
+export default {
+  name: "UserProgramList",
+  components: {NavMenu},
+  data() {
+    return {
+      programs: [],
+      isLoading: true,
+      isLoggedIn: false,
+      user: null,
+    }
+  },
+  mounted() {
+    if (auth.currentUser) {
+      this.isLoggedIn = true
+      this.user = auth.currentUser
+    }
+    let self = this
+    db.collection('programs')
+        .get().then((snapshot) => {
+      snapshot.docs.forEach((r)=>{
+        let data = r.data()
+        if (data.published) {
+          self.programs.push({
+            id: r.id,
+            name: data.name,
+            published: data.published,
+            discipline: data.discipline,
+            creator: data.creator,
+            createdAt: data.createdAt.toDate().toLocaleString()
+          })
+        }
+      })
+      self.isLoading = false
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
